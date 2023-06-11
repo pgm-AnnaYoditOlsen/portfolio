2305d0a005cc0befeefce2edb8371a91d5174956
pgm-AnnaYoditOlsen <annayodit.olsen@student.arteveldehs.be>
2023-06-11 11:40:35 +0200

    Final commit

--- data/projecten.js
+++ data/projecten.js
@@ -3,35 +3,35 @@ const PROJECTS = [
         id: 1,
         image: "images/She-Codes-Africa.png",
         title: "She Code Africa",
-        description: "De website She Code Africa werd gemaakt voor het vak webdesign",
+        description: "De website van She Code Africa werd gemaakt met HTML en CSS.",
         url: ""
     },
     {
         id: 2,
         image: "images/CV.png",
-        title: "Cv",
-        description: "Persoonlijke cv ontworpen voor het vak User Interface",
+        title: "Persoonlijke cv",
+        description: "Persoonlijke cv werd ontworpen in Figma, en uitgewerkt met HTML en CSS.",
         url: ""
     },
     {
         id: 3,
         image: "images/Scrauwen-Groen.png",
         title: "Schrauwen Groen",
-        description: "De website van Schrauwen Groen heb ik gemaakt in de les van AtWork.",
+        description: "Met HTML, CSS en JavaScript heb ik de website van Schrauwen Groen gemaakt.",
         url: "https://pgmgent-atwork-1.github.io/opdracht-1-schrauwen-groen-Anna-Yodit/"
     },
     {
         id: 4,
         image: "images/GentseFeesten.png",
         title: "Gentse Feesten",
-        description: "Voor het vak AtWork heb ik de website van de Gentse Feesten gemaakt.",
+        description: "Voor de website van de Gentse Feesten heb ik gebruik gemaakt van HTML, CSS en JavaScript.",
         url: "https://pgmgent-atwork-1.github.io/opdracht-2-gentse-feesten-2022-Anna-Yodit/"
     },
     {
         id: 5,
         image: "images/Studio-Boost.png",
         title: "Studio Boost",
-        description: "In het vak User Interface Design heb ik de website van Studio Boost gemaaakt.",
+        description: "Studio Boost werd gemaakt met Eleventy.",
         url: ""
     },
 ]
